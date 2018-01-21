3a1e0fe11e67edba29a31958064b06d85bf4378b
Patrik Lamberger <apputvecklaren@gmail.com>
2018-01-21 13:04:48 +0100

    Init
    JS-starter

---
+++ js/script.js
@@ -0,0 +1,23 @@
+/**
+ * App for...
+ * Author: Patrik Lamberger
+ *
+ * @requires ...
+ * @version 0.0.1
+ */
+var app = ( function() {
+    /**** Properties ****/
+    var i = 0;
+
+    /**** Methods ****/
+    function init() {
+       
+    }
+
+    return {
+        init : init
+    };
+  
+} )();
+
+app.init(); // Run application
